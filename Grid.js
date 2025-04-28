725083ad7ed92b1e4741a3dfe11a18156be8d3ee
Damon <dbolesta@users.noreply.github.com>
2025-04-28 17:51:43 -0400

    extracts movement into performAction func

--- Grid.js
+++ Grid.js
@@ -9,8 +9,7 @@ class Grid {
     this.grid = [];
     this.player = new Player("Cool Dude", {hp: 5, atk: 5, def: 6});
 
-    console.log("get stats: ", this.player.getStats());
-
+    // construct initial grid
     for (let x = 0; x <= this.w; x++) {
       let thisRow = [];
       for(let y = 0; y <= this.h; y++) {
@@ -19,51 +18,23 @@ class Grid {
       this.grid.push(thisRow);
     }
 
+    // set player to bottom left
     this.player.x = this.w;
 
     this.start();
   }
 
   async start() {
+    console.clear();
+    console.log("You enter the forest in search of the star!");
+
+    // main loop start
     while(this.player.getStats().hp > 0){
       
       this.logGrid();
       const response = await promptPlayerForDirection();
       console.clear();
-      switch (response) {
-        case "Up":
-          if (this.player.x > 0) {
-            this.grid[this.player.x][this.player.y] = new GridItem(null, true);
-            this.player.x--;
-          } else {
-            console.log("You can't go that way.");
-          }
-          break;
-        case "Down":
-          if (this.player.x < this.w) {
-            this.grid[this.player.x][this.player.y] = new GridItem(null, true);
-            this.player.x++;
-          } else {
-            console.log("You can't go that way.");
-          }
-          break;
-        case "Left":
-          if (this.player.y > 0) {
-            this.grid[this.player.x][this.player.y] = new GridItem(null, true);
-            this.player.y--;
-          } else {
-            console.log("You can't go that way.");
-          }
-          break;
-        case "Right":
-          if (this.player.y < this.h) {
-            this.grid[this.player.x][this.player.y] = new GridItem(null, true);
-            this.player.y++;
-          } else {
-            console.log("You can't go that way.");
-          }
-          break;
-      }
+      this.performAction(response);
 
       console.log(response);
     }
@@ -84,6 +55,43 @@ class Grid {
 
     console.log(gridText);
 
+  }
+
+  performAction(move) {
+    switch (move) {
+      case "Up":
+        if (this.player.x > 0) {
+          this.grid[this.player.x][this.player.y] = new GridItem(null, true);
+          this.player.x--;
+        } else {
+          console.log("You can't go that way.");
+        }
+        break;
+      case "Down":
+        if (this.player.x < this.w) {
+          this.grid[this.player.x][this.player.y] = new GridItem(null, true);
+          this.player.x++;
+        } else {
+          console.log("You can't go that way.");
+        }
+        break;
+      case "Left":
+        if (this.player.y > 0) {
+          this.grid[this.player.x][this.player.y] = new GridItem(null, true);
+          this.player.y--;
+        } else {
+          console.log("You can't go that way.");
+        }
+        break;
+      case "Right":
+        if (this.player.y < this.h) {
+          this.grid[this.player.x][this.player.y] = new GridItem(null, true);
+          this.player.y++;
+        } else {
+          console.log("You can't go that way.");
+        }
+        break;
+    }
   }
 
 
